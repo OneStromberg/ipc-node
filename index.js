1f15413fc7fc0e0920b2b0900c5eaa83c99c8e36
Oleksandr Shtromberg <stro@zajno.com>
2017-06-30 18:55:56 +0300

    improvements

--- index.js
+++ index.js
@@ -3,34 +3,24 @@ var ipc = require('node-ipc');
 ipc.config.id   = 'world';
 ipc.config.retry= 1500;
 
-var nodes = {}
-
+const message = (type, payload = null) => {
+    return {
+        node: ipc.config.id,
+        type: type,
+        payload: payload
+    }
+}
 ipc.serve(
 	function(){
 		ipc.server.on(
 			'message',
 			function(data, socket){
 				ipc.log('got a message : '.debug, Object.keys(data));
-                    //ipc.log('got all required events, telling evryone how muchg I am loved!');
-                    ipc.server.broadcast(
-                        'message',
-                        {
-                            id      : ipc.config.id,
-                            message : data//'Everybody Loves The World! Got messages from hello and goodbye!'
-                        }
-                    );
-				/*ipc.server.emit(
-					socket,
+				//ipc.log('got all required events, telling evryone how muchg I am loved!');
+				ipc.server.broadcast(
 					'message',
 					data
-				);*/
-			}
-		);
-		ipc.server.on(
-			'connect',
-			(data, socket) => {
-				//nodes
-				console.log('connect', Object.keys(data), data.readable);
+				);
 			}
 		);
 		ipc.server.on(
